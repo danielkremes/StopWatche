c73c288ee9e247c15595ea447d2d63e17f5e6386
DanielKremes <danielkremes06@gmail.com>
2024-12-19 19:13:19 -0300

    add Thread and sleep

--- stopwatch/Program.cs
+++ stopwatch/Program.cs
@@ -1,12 +1,17 @@
-﻿
+﻿Start();
 
 static void Start()
 {
     int timer = 10;
     int currentTime = 0;
 
-    while (currentTime != timer) { }
-    currentTime++;
+    while (currentTime != timer)
+    {   
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine("Timer: " + currentTime);
+        Thread.Sleep(1000);
+    }
 }
 
 
