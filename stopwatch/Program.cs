f20929560071bccf244cacfe6b2bef3766a8e1c3
DanielKremes <danielkremes06@gmail.com>
2024-12-19 18:44:45 -0300

    finished function Start()

--- stopwatch/Program.cs
+++ stopwatch/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+static void Start()
+{
+    int timer = 10;
+    int currentTime = 0;
+
+    while (currentTime != timer) { }
+    currentTime++;
+}
+
+
