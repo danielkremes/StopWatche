0711c2408363343b0aedc29cac6d91219d1a8385
DanielKremes <danielkremes06@gmail.com>
2024-12-20 20:26:15 -0300

    made logic to get the option wished by user

--- stopwatch/Program.cs
+++ stopwatch/Program.cs
@@ -2,13 +2,21 @@
 
 static void Menu () {
 
-    Console.WriteLine("S (second)");
-    Console.WriteLine("M (minute)");
+    Console.WriteLine("S (second) 10 second = 10S");
+    Console.WriteLine("M (minute) 10 minute = 10M");
     Console.WriteLine("0 (quit)");
     Console.WriteLine("Timer wished");
 
-    // Start(10);
-}
+    // ToLower =  converted to low case time wished
+    var timerWished = Console.ReadLine()?.ToLower();
+
+    // Get last character in string
+    // in this case the last character is the option
+    // Command bellow give the last character in list
+    // Substring(timerWished.Length - 1, 1);
+    char option = Char.Parse(timerWished.Substring(timerWished.Length - 1, 1));
+    Console.WriteLine($"option wished {option}");
+}   
 
 static void Start(int timer)
 {
--- stopwatch/obj/Debug/net9.0/stopwatch.AssemblyInfo.cs
+++ stopwatch/obj/Debug/net9.0/stopwatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stopwatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73c288ee9e247c15595ea447d2d63e17f5e6386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1243e587d7865f73e14dbb327c27b9b81a93c7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("stopwatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stopwatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
